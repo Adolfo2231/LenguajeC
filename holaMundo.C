a3898698882ae0afe88e9c1d4028e3039ca58282
Adolfo rodriguez <adolfo.rodriguez2231@gmail.com>
2024-06-22 18:50:14 -0400

    Hola Mundo en C

---
+++ holaMundo.C
@@ -0,0 +1,13 @@
+	//Biblioteca, standar input and output//
+#include<stdio.h>
+	
+	//Funcion () parametros
+	int main () {
+		//Mandamos a imprimir a consola
+		
+		printf("Hola Mundo desde C..");
+		
+		return 0;
+	return 0;
+}
+
