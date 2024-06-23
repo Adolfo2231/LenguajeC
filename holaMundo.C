1fbe315eb903e37ebf7778f49698465b6892704b
Adolfo rodriguez <adolfo.rodriguez2231@gmail.com>
2024-06-22 21:04:46 -0400

    Tipos de datos en C parte 2

--- holaMundo.C
+++ holaMundo.C
@@ -1,24 +1,6 @@
 #include <stdio.h>
+#include<stdbool.h>
 
-<<<<<<< HEAD
-int main() {
-	// 1. Declaramos una variable
-	int miNumero;
-	// 2. La inicializamos
-	miNumero = 10;
-	// 3. Imprimir la variable
-	// formato %d -> decimal
-	printf("Mi numero es: %d", miNumero);
-	
-	//1. Declaramos e inicializamos la variableint 
-	int miNumero2 =20, miNumero3 = 30;
-	
-	//2. Imprimir el valor de las variables
-	printf("\nMi numero dos es: %d, numero tres es: %d ",miNumero2, miNumero3);
-=======
-/*
-Tipos de datos en C
-*/
 int main(){
 	
 	//Tipo entero (formato %d, %i)
@@ -32,10 +14,31 @@ int main(){
 	//Tipo double (formato %f, %1f -> long float)
 	double doble = 15.4;
 	printf("\nTipo doble: %.6f", doble);
->>>>>>> master
-
 	
-	return 0;
+	//Tipo char (formato %c)
+	char caracter = 'A';
+	char caracter_decimal = 66;
+	printf("\nTipo caracter: %c, %c", caracter, caracter_decimal);
+	printf("\nTipo caracter en decimal: %d", caracter);
+	
+	//Tipo bool (valores true o false)
+	//true = 1
+	//false = 0
+	bool boleano = false;
+	printf("\nTipo logico: %d", boleano);
+	
+	//Tipo cadena (formato %s)
+	//[]Indica que es un arreglo
+	char cadena [] = "Mi cadena";//\0 ->null al final de una cadena se agrega por ende siempre tendras uno mas de los que vez
+	printf("\nTipo cadena: %s", cadena);
+	
+	//Tipo cadena
+	//[20] define el limite de caracteres permitidos
+	char cadena2[20]="Mi cadena 2";
+	printf("\nTipo cadena2: %s", cadena2);
+	
+	
+		return 0;
 }
 	
 	
