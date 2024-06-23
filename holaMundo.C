230f8a8bd019d90bd474f5d3af3b45bbaf24d72e
Adolfo rodriguez <adolfo.rodriguez2231@gmail.com>
2024-06-22 21:36:07 -0400

    Introducir datos en C parte 1 (direccion de memoria)

--- holaMundo.C
+++ holaMundo.C
@@ -3,39 +3,12 @@
 
 int main(){
 	
-	//Tipo entero (formato %d, %i)
-	int entero=10;
-	printf("Tipo entero: %i", entero);
-	
-	//Tipo flotantte (formato %f) %.2f mostrara solo 2 numero despues del punto
-	float flotante = 3800.5;
-	printf("\nTipo flotante: %.2f", flotante);
-	
-	//Tipo double (formato %f, %1f -> long float)
-	double doble = 15.4;
-	printf("\nTipo doble: %.6f", doble);
-	
-	//Tipo char (formato %c)
-	char caracter = 'A';
-	char caracter_decimal = 66;
-	printf("\nTipo caracter: %c, %c", caracter, caracter_decimal);
-	printf("\nTipo caracter en decimal: %d", caracter);
-	
-	//Tipo bool (valores true o false)
-	//true = 1
-	//false = 0
-	bool boleano = false;
-	printf("\nTipo logico: %d", boleano);
-	
-	//Tipo cadena (formato %s)
-	//[]Indica que es un arreglo
-	char cadena [] = "Mi cadena";//\0 ->null al final de una cadena se agrega por ende siempre tendras uno mas de los que vez
-	printf("\nTipo cadena: %s", cadena);
-	
-	//Tipo cadena
-	//[20] define el limite de caracteres permitidos
-	char cadena2[20]="Mi cadena 2";
-	printf("\nTipo cadena2: %s", cadena2);
+	//1. Declarara variable
+	//En c es neccesario definir una variable para poder usarla
+	//Como buscar direccion de memoria %p(&miNumero)
+	int miNumero = 10;
+	printf("Imprimir el valor de la variable: %d", miNumero);
+	printf("\nImprimir la direccion de memoria: %p", &miNumero);
 	
 	
 		return 0;
