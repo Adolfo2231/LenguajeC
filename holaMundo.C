32cc734ff9f0c8a9579663f3bd25d5d04b734dc5
Adolfo rodriguez <adolfo.rodriguez2231@gmail.com>
2024-06-22 21:53:40 -0400

    Introducir datos en C parte 2

--- holaMundo.C
+++ holaMundo.C
@@ -10,6 +10,17 @@ int main(){
 	printf("Imprimir el valor de la variable: %d", miNumero);
 	printf("\nImprimir la direccion de memoria: %p", &miNumero);
 	
+
+		
+		// 2. Solicitamos al usuario que proporcione un valor
+		printf("\nProporciona un numero: \n");
+		// 3. Leemos la informacion y la asignamos a la variable
+		scanf("%d", &miNumero);
+		// 4. Mandamos a imprimir
+		printf("\nEl numero proporcionado es: %d", miNumero);
+		printf("\nImprimir la dirrecion de memoria: %p", &miNumero);
+	
+	
 	
 		return 0;
 }
