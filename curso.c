537afa982fd7b1c470798e76914f31bd4330eec5
Adolfo rodriguez <adolfo.rodriguez2231@gmail.com>
2024-06-28 12:30:44 -0400

    Operadores de Asignacion en C

--- curso.c
+++ curso.c
@@ -2,34 +2,33 @@
 
 
 int main() {
-	int a, b, c;
-	//Operadores de Incremento: Pre-incremento y Post-incremento)
-	a = 0;
+	//Operadores de Asignacion
+	//Operador = para asignar valar
+	int miNumero = 10;
+	printf("Valor miNumero: %d", miNumero);
+	miNumero = 20;
+	printf("\nValor miNumero: %d", miNumero);
 	
-	//Post-incremento
-	a++;//Se incrementa la proxima vez que utilicemos esta variable
-	printf("Nuevo valor de a: %d", a);
 	
+	//Operador += (Operador compuesto)
+	miNumero += 5; //miNumero = miNumero +5
+	printf("\nValor miNumero: %d", miNumero);
 	
-	//Pre-incremento
-	++a;
-	printf("/nNuevo valor de a: %d", a);
+	//Operador -= (Operador compuesto)
+	miNumero -= 3; //miNumero = miNumero -5
+	printf("\nValor miNumero: %d", miNumero);
 	
-	//ejemplo
-	a = 5;
-	b = 2;
-	c = ++a * b++;
-	printf("\nValor en a: %d",a);//No tenia pendiente ningun incremento
-	printf("\nValor en b: %d",b);//Si tenia pendiente un incremento
-	printf("\nValor en c: %d",c);
+	//Operador *= (Operador compuesto)
+	miNumero *= 2; //miNumero = miNumero *5
+	printf("\nValor miNumero: %d", miNumero);
 	
-	//Operador decremento: Pre-decremento y post decremento
-	a = 5;
-	b = 2;
-	c = --a * b--;
-	printf("\nValor en a: %d",a);//No tenia pendiente ningun decremento 
-	printf("\nValor en b: %d",b);//Si tenia pendiente un decremento 
-	printf("\nValor en c: %d",c);
+	//Operador /= (Operador compuesto)
+	miNumero /= 7; //miNumero = miNumero /5
+	printf("\nValor miNumero: %d", miNumero);
+	
+	//Operador %= (Operador compuesto)
+	miNumero %= 2; //miNumero = miNumero %5
+	printf("\nValor miNumero: %d", miNumero);
 	
 	return 0;
 }
