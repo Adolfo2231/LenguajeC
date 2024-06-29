5d0a771985ce2731cf890b38f634ce1a07010413
Adolfo rodriguez <adolfo.rodriguez2231@gmail.com>
2024-06-29 18:03:35 -0400

    Anadiendole a la carpeta .gitignore curso.c para no seguirla en esta rama

--- curso.c
+++ curso.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <stdbool.h>
-
 int main() {
-	// Operadores Logicos
-	bool a = true;// Verdadero - 1
-	bool b = false;// Falso - 0
-	printf("Valor a: %d", a);
-	printf("\nValor b: %d", b);
-	// Operador Logico && (and o Y)
-	// Regresar verdadero si ambos operandos son verdaderos
-	bool c = a && b;
-	printf("\nResultado operador and: %d", c);
-	// Operador Logico || (or o O)
-	// Regresa verdadero si cualquiera de los operandos es verdadero
-	c = a || b;
-	printf("\nResultado operador or: %d", c);
-	// Operador Logico ! (Not o NO)
-	// Invierte el valor original, de 1 a 0, o de 0 a 1
-	c = !a;
-	printf("\nResultado operador not: %d", c);
+	
+	// Precedencia de Operadores en C
+	// 1. Parentesis y Corchetes
+	// 2. Operadores unarios, como -, ++, --, !
+	// 3. Aritmeticos *, / y %
+	// 4. Aritmeticos + y -
+	// 5. Relacionales <, <=, > y >=
+	// 6. Igualdad == y !=
+	// 7. Logicos && y ||
+	// 8. Asignacion =, =+, -=, *=, etc
+	
+	// Ej. Se revisa de izquierda a derecha
+	int a = 12 / 3 + 2 * 3 - 1;
+	// Paso 1. Division: 12 / 3 = 4
+	// Paso 2. Multiplicacion 2 * 3 = 6
+	// Paso 3. Suma: 4 + 6 = 10
+	// Paso 4. Resta: 10 - 1 = 9
+	printf("Resultado: %d", a);
 	
 	return 0;
 }
