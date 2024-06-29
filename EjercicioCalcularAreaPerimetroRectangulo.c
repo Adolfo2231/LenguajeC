b1caf8b5f8a4b0d1d4efc520b1320ecb431c67a5
Adolfo rodriguez <adolfo.rodriguez2231@gmail.com>
2024-06-29 17:50:22 -0400

    Ejercicio calcular area y perimetro rectangulo

---
+++ EjercicioCalcularAreaPerimetroRectangulo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+	
+	//Ejercicio Calcular area y perimetro rectangulo
+	
+	int base, altura, perimetro, area;
+	
+	printf("Proporcione la base del restangulo: ");
+	scanf("%d", &base);
+	
+	printf("\nProporcione la altura del rectangulo: ");
+	scanf("%d", &altura);
+	
+	area = base * altura;
+	perimetro = 2 * (base * altura);
+	
+	printf("\nArea del rectangulo: %d", area);
+	printf("\nPerimetro del rectangulo: %d", perimetro);
+	return 0;
+}
+
