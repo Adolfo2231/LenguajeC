989b650bc157e81939a5132566fc9e6295f30fa9
Adolfo rodriguez <adolfo.rodriguez2231@gmail.com>
2024-06-29 17:22:33 -0400

    Procedencia de operadores

--- curso.c
+++ curso.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include <stdbool.h>
-
 int main() {
-	// Definimos variables
-	int minimo = 0, maximo = 5;
-	// Solicitamos un valor entre 0 y 5
-	int dato;
-	printf("Proporciona un dato entre 0 y 5: \n");
-	scanf("%d", &dato);
-	// Verificamos si estamos dentro de rango
-	bool dentroRango = dato >= minimo && dato <= maximo;
-	printf("\nValor dentro de rango? %d", dentroRango);
+	
+	// Precedencia de Operadores en C
+	// 1. Parentesis y Corchetes
+	// 2. Operadores unarios, como -, ++, --, !
+	// 3. Aritmeticos *, / y %
+	// 4. Aritmeticos + y -
+	// 5. Relacionales <, <=, > y >=
+	// 6. Igualdad == y !=
+	// 7. Logicos && y ||
+	// 8. Asignacion =, =+, -=, *=, etc
+	
+	// Ej. Se revisa de izquierda a derecha
+	int a = 12 / 3 + 2 * 3 - 1;
+	// Paso 1. Division: 12 / 3 = 4
+	// Paso 2. Multiplicacion 2 * 3 = 6
+	// Paso 3. Suma: 4 + 6 = 10
+	// Paso 4. Resta: 10 - 1 = 9
+	printf("Resultado: %d", a);
 	
 	return 0;
 }
