1faca71cce6336093390cf378ec5267842e4e895
Adolfo rodriguez <adolfo.rodriguez2231@gmail.com>
2024-07-04 12:17:52 -0400

    Nuevo Archivo, Ciclo for

---
+++ Ciclo-for.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	//Ciclo for(Ini.contador,condicion a evaluar, incrementar contador)
+	//Aunque este el var++ la primera vez no pasa
+	for(int contador = 1;contador <= 5; contador++){
+		printf("\nBuenos dias");
+		printf("\nVariable contador: %d\n", contador);
+		
+	}	
+	return 0;
+
+}
+
+
