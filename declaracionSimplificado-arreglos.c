c7ce427a904af24883ae41b34dc64aba6cafe74c
Adolfo rodriguez <adolfo.rodriguez2231@gmail.com>
2024-07-05 16:43:27 -0400

    Simplificacion para declarar un arreglo

---
+++ declaracionSimplificado-arreglos.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    //Solo ponemos la cantidad de arreglos sin tener que espicificarlo
+    int numeros[] = {100, 200, 300, 400};
+    //Conteo empieza desde 0 
+    printf("\nValor 1 Numeros[0] = %d", numeros[0]);
+    printf("\nValor 2 Numeros[1] = %d", numeros[1]);
+    printf("\nValor 3 Numeros[2] = %d", numeros[2]);
+    printf("\nValor 4 Numeros[3] = %d", numeros[3]);
+    return 0;
+}
