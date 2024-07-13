7656f932d9fa419ddde3363e5096d19f71f2bf78
Adolfo rodriguez <adolfo.rodriguez2231@gmail.com>
2024-07-13 16:02:55 -0400

    Nuevo archivo, Convertir texto a numero

---
+++ ConvertirTextoaNumero.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>//libreria std de lenguaje C
+
+int main()
+{
+    // Convertir de texto a numero
+    char cadena1[] = "10";
+    char cadena2[] = "20";
+    // int atoi(const char *string);
+    //  atoi - Ascii to int
+    //  atof - Ascii to float
+    //  Si no es un numero regresa 0
+    int resultado = atoi(cadena1) + atoi(cadena2);
+
+    printf("Resultado suma: %d\n", resultado);
+
+    return 0;
+}
