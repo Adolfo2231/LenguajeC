809a97abbc97e761c4809d07de287596397a84e3
Adolfo rodriguez <adolfo.rodriguez2231@gmail.com>
2024-07-23 21:46:44 -0400

    Nuevo archivo, Ejercicio 2

---
+++ Ejercicio2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//* Agregando texto de escritura en un archivo y tambien lo crea */
+
+FILE *fd;
+int main()
+{
+
+    int c;
+    char direccion[] = "C:\\Users\\ryzen\\OneDrive\\Documents\\Trabajos-C\\hola-mundo\\Curso.txt";
+
+    fd = fopen(direccion, "at"); // at = add text
+
+    if (fd == NULL)
+    {
+        printf("Error al tratar de crear el archivo");
+        return 0
+    }
+
+    while ((c = getchar()) != EOF)
+    {
+        fputc(c, fd);
+    
+    }
+
+    fclose(fd);
+
+    return 0;
+}
