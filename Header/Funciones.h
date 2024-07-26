de5a9c39f04cac6d8094eeecd5044d5f69b781ed
Adolfo rodriguez <adolfo.rodriguez2231@gmail.com>
2024-07-25 22:09:14 -0400

    Se actualizo el nombre a Funciones.h y se agregaron funciones: suma,resta,multiplicacion,division

---
+++ Header/Funciones.h
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int suma(int *pa, int *pb){
+
+    int a = *pa + *pb;
+
+    return a;
+}
+
+int resta(int *pa, int *pb)
+{
+
+    int a = *pa - *pb;
+
+    return a;
+}
+
+int multiplicacion(int *pa, int *pb)
+{
+
+    int a = *pa * *pb;
+
+    return a;
+}
+
+int division(int *pa, int *pb)
+{
+
+    int a = *pa / *pb;
+
+    return a;
+}
