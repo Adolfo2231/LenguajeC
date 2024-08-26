a82f43a1ef2da659cd3a6e8a3a162430ce6910a2
Adolfo Rodriguez <adolfo.rodriguez2231@gmail.com>
2024-08-25 23:27:12 -0400

    add instructing comments

--- OrdenamientoPorInsercion.c
+++ OrdenamientoPorInsercion.c
@@ -1,32 +1,38 @@
 #include <stdio.h>
+#include <stdio.h>
 
 int main()
 {
+    int a[5] = {10, 46, 47, 24, 53}; // Initialize the array
+    int i, pos, aux;                 // Declare variables
 
-    int a[5] = {10, 46, 47, 24, 53};
-    int i, pos, aux;
-
+    // Insertion sort algorithm
     for (i = 0; i < 5; i++)
     {
-        pos = i;
-        aux = a[i];
+        pos = i;    // Set position to current index
+        aux = a[i]; // Store current element in aux
+        // Shift elements to the right to make space for aux
         while ((pos > 0) && (aux < a[pos - 1]))
         {
             a[pos] = a[pos - 1];
             pos--;
         }
-        a[pos] = aux;
+        a[pos] = aux; // Place aux in its correct position
     }
+
+    // Print array in ascending order
     printf("Ascendente\n");
     for (i = 0; i < 5; i++)
     {
         printf("%i, ", a[i]);
     }
+
+    // Print array in descending order
     printf("\nDescendente\n");
     for (i = 4; i >= 0; i--)
     {
         printf("%i, ", a[i]);
     }
 
-    return 0;
+    return 0; // Return 0 to indicate successful execution
 }
