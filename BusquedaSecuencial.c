b6db0a5c72abeb01ecaf0059826e07a712d20fc1
Adolfo Rodriguez <adolfo.rodriguez2231@gmail.com>
2024-08-28 20:53:41 -0400

    File: Busqueda binaria y por seleccion

---
+++ BusquedaSecuencial.c
@@ -0,0 +1,35 @@
+/*
+Busqueda Secuencial en C
+
+a[5] = {3,2,1,4,5};  dato = 4;
+*/
+
+#include <stdio.h>
+
+int main()
+{
+
+    int a[5] = {3, 2, 1, 4, 5};
+    char band = 'F';
+    int i = 0, dato = 3;
+
+    while ((band == 'F') && (i < 5))
+    {
+        if (a[i] == dato)
+        {
+            band = 'V';
+        }
+        i++;
+    }
+
+    if (band == 'F')
+    {
+        printf("El numero no existe en el Array\n");
+    }
+    else if (band == 'V')
+    {
+        printf("El numero existe, posicion: %i\n", i);
+    }
+
+    return 0;
+}
